c3892a9b7807b0e191507feafb649fdd1713de10
koziel101 <hiagokoziel100@gmail.com>
2019-11-27 11:27:32 +0000

    Adding Pizza Exercise

--- FRONTEND/cajondesastre/src/app/Componentes/component-pizza/component-pizza.component.ts
+++ FRONTEND/cajondesastre/src/app/Componentes/component-pizza/component-pizza.component.ts
@@ -13,14 +13,37 @@ export class ComponentPizzaComponent implements OnInit {
   }
 
   pizzaTotal: number = 10;
-  pizzas: string[] = ["Margarita - 2", "Pepperoni - 2", "Havaiana - 4", "Atun - 3", "Sinensia - 10"];
+  pizzas: string[] = ["Basica", "Margarita (+2 euros)", "Pepperoni (+2 euros)", "Havaiana (+4 euros)", "Atun (+3 euros)", "Sinensia (+10 euros)"];
   pizzaSelecionada: string = this.pizzas[0];
+  dobleQueso:boolean = false;
+  qntdPessoas:number = 0;
 
   finalizarPedido() {
+    this.pizzaTotal = 10;
+    
+    //Calculando sabor da Pizza
     switch (this.pizzaSelecionada) {
-      case "Margarita - 2":
-        this.pizzaTotal += 2;
+      case "Margarita (+2 euros)":
+        this.pizzaTotal = this.pizzaTotal + 2;
+        break;
+      case "Pepperoni (+2 euros)":
+        this.pizzaTotal = this.pizzaTotal + 2;
+        break;
+      case "Havaiana (+4 euros)":
+        this.pizzaTotal = this.pizzaTotal + 4;
+        break;
+      case "Atun (+3 euros)":
+        this.pizzaTotal = this.pizzaTotal + 3;
+        break;
+      case "Sinensia (+10 euros)":
+        this.pizzaTotal = this.pizzaTotal + 10;
         break;
     }
+    
+    if(this.dobleQueso){
+      this.pizzaTotal = this.pizzaTotal + 5;
+    }
+    
+    this.pizzaTotal = Number(this.pizzaTotal) + Number(this.qntdPessoas);
   }
 }
