d3a2e75b907cebd3fb7955e38f57aa208129ee9f
Koziel101 <hiagokoziel100@gmail.com>
2019-11-28 18:44:32 +0100

    Adding routing settings for the components

--- FRONTEND/routingdemo/src/app/app-routing.module.ts
+++ FRONTEND/routingdemo/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AltaComponent } from './components/alta/alta.component';
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"home", component:HomeComponent},
+  {path:"alta", component:AltaComponent},
+  {path:"list", component:ListComponent},
+  {path:"edit", component:EditComponent},
+  {path:"**", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
