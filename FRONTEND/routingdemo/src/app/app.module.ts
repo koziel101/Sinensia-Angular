c8f1151db2284d35d3c72c9d8a256c49ee78680b
Koziel101 <hiagokoziel100@gmail.com>
2019-11-28 18:34:02 +0100

    Adding components for routers

--- FRONTEND/routingdemo/src/app/app.module.ts
+++ FRONTEND/routingdemo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AltaComponent } from './components/alta/alta.component';
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AltaComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
